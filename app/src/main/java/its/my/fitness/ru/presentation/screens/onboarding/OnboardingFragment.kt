9aca330f87ffac53c738cadf61f435db639de57d
daniel <meh.daniel@yandex.com>
2023-02-18 14:43:13 +0700

    созданный onboarding экран

--- app/src/main/java/its/my/fitness/ru/presentation/screens/onboarding/OnboardingFragment.kt
+++ app/src/main/java/its/my/fitness/ru/presentation/screens/onboarding/OnboardingFragment.kt
@@ -2,6 +2,7 @@ package its.my.fitness.ru.presentation.screens.onboarding
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import its.my.fitness.ru.R
 import its.my.fitness.ru.databinding.OnboardingFragmentBinding
 import its.my.fitness.ru.presentation.base.BaseFragment
@@ -14,8 +15,30 @@ class OnboardingFragment: BaseFragment<OnboardingViewModel, OnboardingFragmentBi
     override fun initBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): OnboardingFragmentBinding {
-        TODO("Not yet implemented")
+    ): OnboardingFragmentBinding = OnboardingFragmentBinding.inflate(inflater)
+
+    override fun initialize() {
+        var number = 1
+        with(binding){
+            onboardingButton.setOnClickListener {
+                number++
+                if (number == 2) {
+                    onboardingImage.setImageResource(R.drawable.onboarding_2)
+                    onboardingButton.setImageResource(R.drawable.onboarding_button_2)
+                }
+                if (number == 3) {
+                    onboardingImage.setImageResource(R.drawable.onboarding_3)
+                    onboardingButton.setImageResource(R.drawable.onboarding_button_3)
+                }
+                if (number == 4) {
+                    onboardingImage.setImageResource(R.drawable.onboarding_4)
+                    onboardingButton.setImageResource(R.drawable.onboarding_button_4)
+                }
+                if (number == 5) {
+
+                }
+            }
+        }
     }
 
 }
